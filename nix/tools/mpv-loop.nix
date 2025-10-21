220cbc0f555ee9de290d3fee09f05b3fa7f325e4
Nick Spinale <nick@nickspinale.com>
2025-10-20 23:08:44 -0700

    works

--- nix/tools/mpv-loop.nix
+++ nix/tools/mpv-loop.nix
@@ -6,27 +6,45 @@
 , uosc
 
 , toolsDir
+
+, runCommand
 }:
 
-buildLua {
+let
+  dir = "${toolsDir + "/from-uosc"}";
+
+  copied = runCommand "x" {} ''
+    mkdir $out
+    cp -r ${dir} $out/uosc
+  '';
+in
+
+buildLua (finalAttrs: {
   pname = "mpv-tt-loop";
   version = "0.0.1";
 
   dontUnpack = true;
   # scriptPath = "${toolsDir + "/mpv-loop"}";
-  scriptPath = "${toolsDir + "/mpv-loop-uosc"}";
+  # scriptPath = "${toolsDir + "/from-uosc"}";
+  # scriptPath = "${uosc}/share/mpv/scripts/uosc";
+  scriptPath = "${copied}/uosc";
+
+  postInstall = ''
+    cp -r ${uosc}/share/fonts $out/share
+  '';
 
   passthru.extraWrapperArgs = [
     "--set"
       "FONTCONFIG_FILE"
       (toString (makeFontsConf {
         fontDirectories = [
-          "${materialDesignIconicFont}/share/fonts"
-          "${uosc}/share/fonts"
+          # "${materialDesignIconicFont}/share/fonts"
+          # "${uosc}/share/fonts"
+          "${finalAttrs.finalPackage}/share/fonts"
         ];
       }))
     "--set"
       "MPV_UOSC_ZIGGY"
       (lib.getExe' uosc.tools "ziggy")
   ];
-}
+})
--- tools/from-uosc/elements/Controls.lua
+++ tools/from-uosc/elements/Controls.lua
@@ -12,7 +12,7 @@ local Speed = require('elements/Speed')
 -- scale - `options.controls_size` scale factor.
 -- ratio - Width/height ratio of a static or dynamic element.
 -- ratio_min Min ratio for 'dynamic' sized element.
----@alias ControlItem {element?: Element; kind: string; sizing: 'space' | 'static' | 'dynamic' | 'gap'; scale: number; ratio?: number; ratio_min?: number; hide: boolean; dispositions?: {[string]: boolean}[]}
+---@alias ControlItem {element?: Element; kind: string; sizing: 'space' | 'static' | 'dynamic' | 'gap'; scale: number; ratio?: number; ratio_min?: number; hide: boolean; dispositions?: table<string, boolean>}
 
 ---@class Controls : Element
 local Controls = class(Element)
@@ -93,27 +93,15 @@ function Controls:init_options()
 		local parts = split(config, ' *: *')
 		local kind, params = parts[1], itable_slice(parts, 2)
 
-		-- Serialize dispositions into OR groups of AND conditions
-		---@type {[string]: boolean}[]
+		-- Serialize dispositions
 		local dispositions = {}
-		---@type string[]
-		local disposition_props = {}
-		for _, or_group in ipairs(comma_split(item.disposition)) do
-			local group = {}
-			for _, condition in ipairs(split(or_group, ' *+ *')) do
-				if #condition > 0 then
-					local value = condition:sub(1, 1) ~= '!'
-					local name = not value and condition:sub(2) or condition
-					if name:sub(1, 4) == 'has_' or itable_has({'idle', 'image', 'audio', 'video', 'stream'}, name) then
-						local prop = name:sub(1, 4) == 'has_' and name or 'is_' .. name
-						group[prop] = value
-					else
-						disposition_props[#disposition_props + 1] = name
-						group[name] = value
-					end
-				end
+		for _, definition in ipairs(comma_split(item.disposition)) do
+			if #definition > 0 then
+				local value = definition:sub(1, 1) ~= '!'
+				local name = not value and definition:sub(2) or definition
+				local prop = name:sub(1, 4) == 'has_' and name or 'is_' .. name
+				dispositions[prop] = value
 			end
-			dispositions[#dispositions + 1] = group
 		end
 
 		-- Convert toggles into cycles
@@ -187,7 +175,6 @@ function Controls:init_options()
 					name = params[1],
 					render_order = self.render_order,
 					anchor_id = 'controls',
-					on_hide = function() self:reflow() end,
 				})
 				table_assign(control, {element = element, sizing = 'static', scale = 1, ratio = 1})
 			end
@@ -206,11 +193,6 @@ function Controls:init_options()
 			break
 		end
 
-		if control.element then
-			for _, prop in ipairs(disposition_props) do
-				control.element:observe_mp_property(prop, function() self:reflow() end)
-			end
-		end
 		self.controls[#self.controls + 1] = control
 	end
 
@@ -218,39 +200,18 @@ function Controls:init_options()
 end
 
 function Controls:reflow()
-	-- Populate the layout only with items that are not hidden and match current disposition
+	-- Populate the layout only with items that match current disposition
 	self.layout = {}
 	for _, control in ipairs(self.controls) do
-		local matches = false
-		local conditions_num = 0
-
-		-- Check against OR groups of AND conditions
-		for _, group in pairs(control.dispositions) do
-			local group_matches = true
-			for prop, value in pairs(group) do
-				conditions_num = conditions_num + 1
-				---@type boolean
-				local current_value
-				if prop:sub(1, 4) == 'has_' or prop:sub(1, 3) == 'is_' then
-					current_value = state[prop]
-				else
-					current_value = mp.get_property_bool(prop, false)
-				end
-				if current_value ~= value then
-					group_matches = false
-					break
-				end
-			end
-			if group_matches then
-				matches = true
+		local matches = true
+		for prop, value in pairs(control.dispositions) do
+			if state[prop] ~= value then
+				matches = false
 				break
 			end
 		end
-
-		if conditions_num == 0 then matches = true end
-		local show = matches and (not control.element or control.element.hide ~= true)
-		if control.element then control.element.enabled = show end
-		if show then self.layout[#self.layout + 1] = control end
+		if control.element then control.element.enabled = matches end
+		if matches then self.layout[#self.layout + 1] = control end
 	end
 
 	self:update_dimensions()
@@ -289,7 +250,7 @@ function Controls:register_badge_updater(badge, element)
 	if is_external_prop then
 		element['on_external_prop_' .. prop] = function(_, value) handler(prop, value) end
 	else
-		element:observe_mp_property(observable_name, handler)
+		self:observe_mp_property(observable_name, handler)
 	end
 end
 
